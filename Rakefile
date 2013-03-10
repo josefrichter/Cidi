2debe8927e61fbf439d84ef084f7ece4f9cc4db7
Josef Richter <josef.richter@me.com>
2013-03-11 00:41:10 +0100

    distance caluclation, but incorrect

--- Rakefile
+++ Rakefile
@@ -2,6 +2,7 @@
 $:.unshift("/Library/RubyMotion/lib")
 require 'motion/project'
 require 'bubble-wrap'
+require 'bubble-wrap/location'
 
 Motion::Project::App.setup do |app|
   # Use `rake config' to see complete project settings.
--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,6 +1,6 @@
 class CitiesController < UIViewController
 
-  path = NSBundle.mainBundle.pathForResource("cidi_cities100", ofType:"json")
+  path = NSBundle.mainBundle.pathForResource("cidi_cities1000", ofType:"json")
   CITIES = BW::JSON.parse NSData.dataWithContentsOfFile(path)
 
   def viewDidLoad
@@ -42,15 +42,51 @@ class CitiesController < UIViewController
     controller.view.backgroundColor = UIColor.whiteColor
     controller.title = city
 
-    label = UILabel.alloc.initWithFrame(CGRectZero)
-    label.text = city
-    label.sizeToFit
-    label.center = [controller.view.frame.size.width/2,
+    @label = UILabel.alloc.initWithFrame(CGRectZero)
+    @label.text = "0"
+    @label.sizeToFit
+    @label.center = [controller.view.frame.size.width/2,
       controller.view.frame.size.height/2]
-    controller.view.addSubview(label)
+
+    # docs: https://github.com/rubymotion/BubbleWrap/blob/master/motion/location/location.rb
+    BW::Location.get do |result|
+      dist = distcalc(result[:to].latitude, result[:to].longitude, 
+        CITIES[indexPath.row][3], CITIES[indexPath.row][4])
+      p dist
+      @label.text = dist.to_s
+      @label.sizeToFit
+    end
+
+    controller.view.addSubview(@label)
     self.navigationController.pushViewController(controller, animated:true)
   end
 
+  def distcalc(lat1,lon1,lat2,lon2)
+    #p lat1, lon1, lat2, lon2
+    lat2 = todeg(lat2)
+    lon2 = todeg(lon2)
+    r = 6371; # km
+
+    dLat = torad(lat2-lat1)
+    dLon = torad(lon2-lon1)
+
+    lat1 = torad(lat1)
+    lat2 = torad(lat2)
+
+    a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+            Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2) 
+    c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)) 
+    d = r * c
+    return d.round
+  end
+
+  def torad(deg)
+    deg * Math::PI / 180
+  end
+
+  def todeg(rad)
+    rad / Math::PI / 180
+  end
 
 end
 
