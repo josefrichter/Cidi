ec2b65d5a952d06e481c12c71ce09fe785d0a264
Josef Richter <josef.richter@me.com>
2013-03-11 18:49:49 +0100

    tests wip

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -6,4 +6,9 @@ describe "Application 'Cidi'" do
   it "has one window" do
     @app.windows.size.should == 1
   end
+
+  it "has a root view controller" do
+    @app.keyWindow.rootViewController.class.should == CitiesController
+  end
+
 end
