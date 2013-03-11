30d56b37a13821554626de167773b92ff4171e90
Josef Richter <josef.richter@me.com>
2013-03-11 20:26:54 +0100

    more tests, failing

--- spec/cities_controller_spec.rb
+++ spec/cities_controller_spec.rb
@@ -8,4 +8,22 @@ describe "CitiesController" do
   it "shows first city in first cell" do
     views(UITableViewCell)[0].text.should == CitiesController::CITIES[0][:name]
   end
+
+  it "shows all cities in a table" do
+    CitiesController::CITIES.length.should == controller.tableView.numberOfRowsInSection(0)
+  end
+
+  it "should have disclosure indicators" do
+    views(UITableViewCell).first.accessoryType.should == UITableViewCellAccessoryDisclosureIndicator
+  end
+
+  it "shows detail screen for a table cell" do
+    view("Accra").should.not.be.nil
+    tap("Accra")
+    wait 2 do
+      tap("OK")
+    end
+    0.should == 0
+  end
+
 end
